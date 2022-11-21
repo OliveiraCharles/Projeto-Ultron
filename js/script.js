a8e9bfb7208b0da5f8b0c404b7db8376a3afa51e
Leonardo-ReisSouza-Facens <leonardo.souza@facens.br>
2022-11-21 18:25:22 -0300

    caixa de pesquisa adicionada

--- js/script.js
+++ js/script.js
@@ -31,3 +31,23 @@ controls.forEach((control) => {
     items[currentItem].classList.add("current-item");
   });
 });
+
+function Pesquisa(){
+  var entrada, filtro, ul, li, a, i, valorTexto;
+  entrada = document.getElementById('entradaPesquisa')
+
+  filtro = entrada.value.toUpperCase();
+  ul = document.getElementById("listaPesquisa");
+  li = ul.getElementsByTagName('li');
+
+  for (i=0; i< li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    valorTexto = a.textContent || a.innerText;
+    if(valorTexto.toUpperCase().indexOf(filtro) > -1) {
+      li[i].style.display = "";
+    } else{
+      li[i].style.display = "none";
+    }
+  }
+
+}
